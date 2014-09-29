c469eab8bb30a09f862933bbb41b7eb332cc25d8
Rick Heller <rick.heller@yahoo.com>
2014-09-29 17:44:32 -0400

    added base url to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = {host: 'thawing-mountain-9297.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
