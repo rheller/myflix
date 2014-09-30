8cf7b70fd0a6c07331157c3861103960ef2de0c4
Rick Heller <rick.heller@yahoo.com>
2014-09-30 14:49:29 -0400

    added comments

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,7 +9,8 @@ class InvitationsController < ApplicationController
   def create
     @invitation = Invitation.new(invitation_params.merge!(inviter_id: current_user.id) )
     if @invitation.save
-      AppMailer.delay.invite(@invitation)
+      #AppMailer.delay.invite(@invitation)   #immediate
+      AppMailer.delay.invite(@invitation)    #background via sidekiq
       flash[:notice] = 'Invitation Sent'
       redirect_to new_invitation_path
     else
