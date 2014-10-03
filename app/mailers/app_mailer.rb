47c701a6e0536ee623dcb0851f511991b80491f1
Rick Heller <rick.heller@yahoo.com>
2014-10-02 21:54:35 -0400

    fix error in email per environments

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,24 +1,23 @@
 class AppMailer < ActionMailer::Base
 
-
   def send_forgot_password(user)
     @user = user
-    mail from: 'info@myflix.com', to: find_address, subject: "Password Reset" 
+    mail from: 'info@myflix.com', to: find_address(@user), subject: "Password Reset" 
   end
 
   def notify_on_new(user)
     @user = user
-    mail from: 'info@myflix.com', to: find_address, subject: "Welcome to RickMyflix" 
+    mail from: 'info@myflix.com', to: find_address(@user), subject: "Welcome to RickMyflix" 
   end
 
   def invite(invitation)
     @invitation = invitation
-    mail from: 'info@myflix.com', to: find_address, subject: "Check out RickMyflix" 
+    mail from: 'info@myflix.com', to: find_address(@invitation), subject: "Check out RickMyflix" 
   end
 
-  def find_address
+  def find_address(item)
     if ENV["REAL_PRODUCTION"] == 'real_production'
-      @user.email
+      item.email
     else
       'rick.heller@yahoo.com' #send to admin if staging, dev, or test 
     end
