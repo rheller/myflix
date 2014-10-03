b2fe406f67eeff3ebead932bfdfa1e5d8b126aba
Rick Heller <rick.heller@yahoo.com>
2014-10-02 21:35:48 -0400

    make email only if real production dry

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,20 +3,25 @@ class AppMailer < ActionMailer::Base
 
   def send_forgot_password(user)
     @user = user
-    address = (ENV["REAL_PRODUCTION"] == 'real_production')  ? @user.email  : 'rick.heller@yahoo.com' 
-    mail from: 'info@myflix.com', to: address, subject: "Password Reset" 
+    mail from: 'info@myflix.com', to: find_address, subject: "Password Reset" 
   end
 
   def notify_on_new(user)
     @user = user
-    address = (ENV["REAL_PRODUCTION"] == 'real_production')  ? @user.email  : 'rick.heller@yahoo.com' 
-    mail from: 'info@myflix.com', to: address, subject: "Welcome to RickMyflix" 
+    mail from: 'info@myflix.com', to: find_address, subject: "Welcome to RickMyflix" 
   end
 
   def invite(invitation)
     @invitation = invitation
-    address = (ENV["REAL_PRODUCTION"] == 'real_production')  ? @user.email  : 'rick.heller@yahoo.com' 
-    mail from: 'info@myflix.com', to: address, subject: "Check out RickMyflix" 
+    mail from: 'info@myflix.com', to: find_address, subject: "Check out RickMyflix" 
+  end
+
+  def find_address
+    if ENV["REAL_PRODUCTION"] == 'real_production'
+      @user.email
+    else
+      'rick.heller@yahoo.com' #send to admin if staging, dev, or test 
+    end
   end
 
 end
