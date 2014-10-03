92be3d4fef261e462c4f6f4e82079bd8adf21efa
Rick Heller <rick.heller@yahoo.com>
2014-10-02 21:26:29 -0400

    add env variable to distinguish real production

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,22 +3,20 @@ class AppMailer < ActionMailer::Base
 
   def send_forgot_password(user)
     @user = user
-    mail from: 'info@myflix.com', to: 'rick.heller@yahoo.com', subject: "Password Reset" 
+    address = (ENV["REAL_PRODUCTION"] == 'real_production')  ? @user.email  : 'rick.heller@yahoo.com' 
+    mail from: 'info@myflix.com', to: address, subject: "Password Reset" 
   end
 
   def notify_on_new(user)
     @user = user
-#dont sent emails to random users, send to my yahoo account.
-    #mail from: 'info@myflix.com', to: user.email, subject: "Welcome to RickMyflix" 
-    mail from: 'info@myflix.com', to: 'rick.heller@yahoo.com', subject: "Welcome to RickMyflix" 
+    address = (ENV["REAL_PRODUCTION"] == 'real_production')  ? @user.email  : 'rick.heller@yahoo.com' 
+    mail from: 'info@myflix.com', to: address, subject: "Welcome to RickMyflix" 
   end
 
   def invite(invitation)
     @invitation = invitation
-#dont sent emails to random users, send to my yahoo account.
-    #mail from: 'info@myflix.com', to: user.email, subject: "Welcome to RickMyflix" 
-    mail from: 'info@myflix.com', to: 'rick.heller@yahoo.com', subject: "Check out RickMyflix" 
+    address = (ENV["REAL_PRODUCTION"] == 'real_production')  ? @user.email  : 'rick.heller@yahoo.com' 
+    mail from: 'info@myflix.com', to: address, subject: "Check out RickMyflix" 
   end
-
 
 end
