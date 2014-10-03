ec07205923c9db2154654ce78ce85370e7bfdf11
Rick Heller <rick.heller@yahoo.com>
2014-10-02 22:04:58 -0400

    fix one further error

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,22 +2,22 @@ class AppMailer < ActionMailer::Base
 
   def send_forgot_password(user)
     @user = user
-    mail from: 'info@myflix.com', to: find_address(@user), subject: "Password Reset" 
+    mail from: 'info@myflix.com', to: find_address(@user.email), subject: "Password Reset" 
   end
 
   def notify_on_new(user)
     @user = user
-    mail from: 'info@myflix.com', to: find_address(@user), subject: "Welcome to RickMyflix" 
+    mail from: 'info@myflix.com', to: find_address(@user.email), subject: "Welcome to RickMyflix" 
   end
 
   def invite(invitation)
     @invitation = invitation
-    mail from: 'info@myflix.com', to: find_address(@invitation), subject: "Check out RickMyflix" 
+    mail from: 'info@myflix.com', to: find_address(@invitation.recipient_email), subject: "Check out RickMyflix" 
   end
 
-  def find_address(item)
+  def find_address(address)
     if ENV["REAL_PRODUCTION"] == 'real_production'
-      item.email
+      address
     else
       'rick.heller@yahoo.com' #send to admin if staging, dev, or test 
     end
