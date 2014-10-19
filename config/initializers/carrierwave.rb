af28ec9e8cb5c720da8cbbc621e79d61feba00b7
Rick Heller <rick.heller@yahoo.com>
2014-10-19 18:14:55 -0400

    add s3 bucket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     }
-    config.fog_directory  = 'name_of_directory'                          # required
+    config.fog_directory  = 'ricktealeaf'                          # required
     #config.fog_public     = false                                        # optional, defaults to true
     #config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
   else
