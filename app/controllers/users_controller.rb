b4ddefdcead1115cce3ca5bd91380e3467cae793
Rick Heller <rick.heller@yahoo.com>
2014-11-24 17:29:14 -0500

    add service for user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,61 +17,24 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.valid?
-
-# the token is just a link to the potential charge. This charges it.
-      response = StripeWrapper::Charge.create(
-        :amount => params[:amount],
-        :currency => "usd",
-        :card => params[:stripeToken],
-        :description => "RickFlix Charge"
-        )
-      if  response.successful?
-         @user.save
-         AppMailer.notify_on_new(@user).deliver   #immediately
-
-#      background processing requires 
-#        redis-server /usr/local/etc/redis.conf
-#        foreman start
-#        http://localhost:5000/
-#         AppMailer.delay.notify_on_new(@user)      #background via sidekiq
-         handle_invitation
-         redirect_to sign_in_path, notice: "Thank you for registering. Please sign in."
-      else
-        flash[:error] = response.error_message
-        render "new"
-      end
+    response = UserSignUp.new(@user).sign_up(params)
+    if response.successful?
+      flash[:success] = "Thank you for joining!"
+      redirect_to sign_in_path, notice: "Thank you for registering. Please sign in."
     else
-      flash[:error] = "The user information looks invalid."
+      flash[:error] = response.error_message
       render "new"
     end
+
   end
 
   def show
     @user = User.find_by_id(params[:id])
   end 
 
-
-  private
-
-    def handle_invitation
-       token = params[:invitation_token]
-       if token.present?
-         invitation = Invitation.where(token: token).first
-         if invitation.present?
-           inviter = invitation.inviter
-           if inviter.present?
-             inviter.follow(@user)
-             @user.follow(inviter)
-             invitation.update_column(:token, nil)
-           end
-         end
-       end
-    end
-
-    def user_params
-      params.require(:user).permit(:email, :full_name, :password)
-    end
+  def user_params
+     params.require(:user).permit(:email, :full_name, :password)
+  end
 
  
 end
---
+++ app/services/user_sign_up.rb
@@ -0,0 +1,62 @@
+class UserSignUp
+
+  attr_reader :user, :error_message
+
+  def initialize(user)
+    @user = user
+  end
+
+  def sign_up(params)
+    if user.valid?
+      response = StripeWrapper::Charge.create(
+        :amount => params[:amount],
+        :currency => "usd",
+        :card => params[:stripeToken],
+        :description => "RickFlix Charge"
+        )
+      if response.successful?
+         user.save
+         AppMailer.notify_on_new(user).deliver   #immediately
+
+#      background processing requires 
+#        redis-server /usr/local/etc/redis.conf
+#        foreman start
+#        http://localhost:5000/
+#         AppMailer.delay.notify_on_new(@user)      #background via sidekiq
+         handle_invitation(params)
+         @status = :successful
+         self
+      else
+        @status = :failed
+        @error_message = response.error_message 
+        self
+      end
+    else
+      @status = :failed
+      @error_message = "The user information looks invalid."
+      self
+    end
+  end
+
+  def successful?
+    @status == :successful
+  end
+
+    private
+
+    def handle_invitation(params)
+       token = params[:invitation_token]
+       if token.present?
+         invitation = Invitation.where(token: token).first
+         if invitation.present?
+           inviter = invitation.inviter
+           if inviter.present?
+             inviter.follow(@user)
+             @user.follow(inviter)
+             invitation.update_column(:token, nil)
+           end
+         end
+       end
+    end
+
+end
